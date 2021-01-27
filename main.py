06ac1cdc04afcfdfb86a8aae71eea93ea5a03c79
conormai <conormai@tamu.edu>
2021-01-26 23:15:42 -0600

    created a main executable file that would read and write to files

---
+++ main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import sys
+import getopt
+import os
+
+def main(argv):
+
+    inputfile = ""
+    outputfile = ""
+
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print('./'+os.path.basename(__file__)+' -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('./'+os.path.basename(__file__)+' -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+
+    try:
+        with open(inputfile, "r") as f:
+            with open(outputfile, "w") as out:
+                lines = f.readlines()
+                out.write(str(lines))
+    except OSError:
+        print("Could not open/read file:", inputfile)
+        sys.exit()
+        
+
+        
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
